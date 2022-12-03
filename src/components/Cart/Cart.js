449b66ab253428498fda41f97b522edafb1a02f9
Kajsia Anderson <kajsia.anderson@gmail.com>
2022-12-03 12:51:05 -0700

    added close cart functionality

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,14 +7,14 @@ const Cart = (props) => {
         {[{ id: 'c1', name: 'sushi', amount: 2, price: 12.99 },].map((item) => <li>{item.name}</li>)}</ul>
 
     return (
-        <Modal>
+        <Modal onClose={props.onClose}>
             {cartItems}
             <div className={styles.total}>
                 <span>Total Amount</span>
                 <span>35.62</span>
             </div>
             <div className={styles.actions}>
-                <button className={styles['button--alt']}>Close</button>
+                <button className={styles['button--alt']} onClick={props.onClose}>Close</button>
                 <button className={styles.button}>Order</button>
             </div>
         </Modal>
